39d8b7fa2385180b05452db9dd1361588ab54ffd
WizzyCodes <wisdomiberi832@gmail.com>
2024-12-28 08:23:25 +0100

    update

--- dist/main.js
+++ dist/main.js
@@ -20,7 +20,7 @@ const main = (app) => __awaiter(void 0, void 0, void 0, function* () {
         app.get("/", (req, res) => {
             try {
                 return res.status(200).json({
-                    message: "Welcome To Wisdom's Tech-Hack Challenge",
+                    message: "Welcome To Praise Tech Challenge",
                 });
             }
             catch (error) {
