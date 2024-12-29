20a91bee67c63159e2ea40aeb90b9bdf5e184c96
WizzyCodes <wisdomiberi832@gmail.com>
2024-12-30 00:14:08 +0100

    uodated

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
@@ -13,6 +15,10 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 (0, main_1.main)(app);
+
+app.set("views", path.join(__dirname, "../views"));
+
+app.set("view engine", "ejs");
 app.listen(parseInt(process.env.PORT), () => {
-    (0, dbConfig_1.dbConfig)();
+  (0, dbConfig_1.dbConfig)();
 });
