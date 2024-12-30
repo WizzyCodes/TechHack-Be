8653b4a5734b8062e97e16668ab7526a5a390846
WizzyCodes <wisdomiberi832@gmail.com>
2024-12-30 01:41:01 +0100

    views in dist

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,9 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const path = require("path");
-
 const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const main_1 = require("./main");
@@ -17,10 +13,6 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 (0, main_1.main)(app);
-
-app.set("views", path.join(__dirname, "../views"));
-
-app.set("view engine", "ejs");
 app.listen(parseInt(process.env.PORT), () => {
-  (0, dbConfig_1.dbConfig)();
+    (0, dbConfig_1.dbConfig)();
 });
